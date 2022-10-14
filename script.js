f4d52576cae2754c55a686a927baf731277a93bd
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2022-10-13 21:02:59 -0300

    ...

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+let produtos = [
+    {description: "Uva", price: 4.50},
+    {description: "Goiaba", price: 3.20},
+    {description: "Banana", price: 4.60},
+    {description: "Maçã", price: 3.30},
+    {description: "Laranja", price: 5.70},   
+    {description: "Abacaxi", price: 7.00}
+]
+    let addProduto = [];
+        const listaProd = document.querySelector("#lista-produtos");
+        const totalComp = document.querySelector("#compra-cliente");
+        const valorComp = document.querySelector("#valor-compra");  
+}
